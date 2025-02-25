2a5c99793528c799c2b8873ce1d4522e9edd1305
mohamad <mo.alharbi077@gmail.com>
2025-02-25 13:52:52 +0300

    solve lab view

--- CarsRental/main/urls.py
+++ CarsRental/main/urls.py
@@ -6,6 +6,6 @@ app_name = 'main'
 urlpatterns = [
     path('',views.home_view,name='home_view'),
     path('about/',views.about_view,name='about_view'),
-    path('password/generate',views.generate_password_view,name='generate_password_view')
+    path('password/generate/',views.generate_password_view,name='generate_password_view')
     
 ]
