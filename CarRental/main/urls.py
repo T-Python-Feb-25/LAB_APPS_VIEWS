e08ac82f04306feb10bd8b480e171db23478547c
Ahmed Alghamdi <moodya793@gmail.com>
2025-02-25 23:53:53 +0300

    rename

--- CarRental/main/urls.py
+++ CarRental/main/urls.py
@@ -5,7 +5,7 @@ app_name = "main"
 
 urlpatterns = [
     path("", views.home_view, name="home_view"),
-    path("about/us/", views.about_view, name="about_view"),
+    path("about/", views.about_view, name="about_view"),
     path('', views.generate_password, name='home'),
     path('password/generate/', views.generate_password, name='generate_password'),
 ]
