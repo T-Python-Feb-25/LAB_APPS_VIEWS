066b47d84de434b9a6f404df0a2dc3ae438dba9b
eman rajhi <emyksa1429@gmail.com>
2025-02-25 22:29:40 +0300

    add solution bonus

--- CarsRental/app_cars/urls.py
+++ CarsRental/app_cars/urls.py
@@ -6,6 +6,7 @@ from . import views
 app_cars = "cars"
 urlpatterns = [
    path("", views.page_view, name="page_view"),
-   path("about/",  views.about_view, name="about_view")
+   path("about/",  views.about_view, name="about_view"),
+   path("password/generate/",  views.generate_password , name="generate_password"),
    
  ]
