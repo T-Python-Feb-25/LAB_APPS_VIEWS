ca9bb3f775437a9a54f0e088d710f92582452ec5
Ahmed Alghamdi <moodya793@gmail.com>
2025-02-25 14:21:24 +0300

    lab+bouns

---
+++ CarRental/main/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+app_name = "main"
+
+urlpatterns = [
+    path("", views.home_view, name="home_view"),
+    path("about/us/", views.about_view, name="about_view"),
+    path('', views.generate_password, name='home'),
+    path('password/generate/', views.generate_password, name='generate_password'),
+]
